138aded2bef83aac1b0f552eade8d37d6f3e9c51
timmy3000 <hatimk916@gmail.com>
2023-01-12 05:37:54 +0200

    Clone Repo

---
+++ src/pages/APIDesc.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+import {
+  AiModels,
+  Brands,
+  Discover,
+  Features,
+  Hero,
+  Market,
+  Navbar,
+  Pricing,
+  Blog,
+  GettingStarted,
+  Footer
+} from "../components";
+
+const Home = () => {
+  return (
+    <Stack direction="column">
+      <Navbar />
+      <Hero />
+      <Brands />
+      <AiModels />
+      <Discover />
+      <Features />
+      <Market />
+      <Pricing />
+      <Blog />
+      <GettingStarted />
+      <Footer />
+    </Stack>
+  );
+};
+
+export default Home;
