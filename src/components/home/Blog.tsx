11c3cf1ee65f41755cc941ed22df7d72c9b91c8a
Vinyl-Davyl <okononfuadavid@gmail.com>
2023-01-15 10:40:35 +0100

    Updated icons and designs surronding the Illustr bright on dark mode

--- src/components/home/Blog.tsx
+++ src/components/home/Blog.tsx
@@ -6,7 +6,7 @@ import { FiArrowRight } from "react-icons/fi";
 import { Box, Card, CardContent, CardMedia, Stack, Theme, Typography } from "@mui/material";
 
 import { useAppContext } from "../../contexts/AppProvider";
-import {blogIllustration, blogPost1,  blogPost2} from "../../assets/svg";
+import {blogIllustrationLight, blogIllustrationDark, blogPost1,  blogPost2} from "../../assets/svg";
 
 const blogPosts = [
   {
@@ -52,8 +52,8 @@ const Blog: React.FC = () => {
       </Box>
       <Box className={classes.container2}>
         <Typography variant="h1">AI Education</Typography>
-        <Typography>Explore our blog posts to get more insight on AI.</Typography>
-        <img src={blogIllustration} alt="" width="100%" height="100%" />
+        <Typography style={{borderColor: currentMode === "dark" ? "#F5F5F5" : "#060607",color: currentMode === "dark" ? "#F5F5F5" : "#060607"}}>Explore our blog posts to get more insight on AI.</Typography>
+        <img src={currentMode === "dark" ? blogIllustrationDark : blogIllustrationLight} alt="" width="100%" height="100%" />
       </Box>
     </Box>
   );
