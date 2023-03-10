7fe9b794e0ca0384909e78086110ddf9bbcfd0ca
Taiwo Akindele <akindeletaiwo214@gmail.com>
2023-03-10 15:15:51 +0100

    used cookies data on dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,22 @@
 import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import Cookies from "universal-cookie";
 
 const REDIRECT_URL = import.meta.env.VITE_REDIRECT_URL as string;
 
 const Dashboard = () => {
   const [searchParams] = useSearchParams();
-  const id = searchParams.get("id")
-  const token = searchParams.get("token")
+  const id = searchParams.get("id");
+  const token = searchParams.get("token");
+  const cookies = new Cookies();
 
   useEffect(() => {
-    window.location.replace(`${REDIRECT_URL}?id=${id}&token=${token}`)
-  },[])
+    const storedId = cookies.get("profileId");
+    const storedToken = cookies.get("accessToken");
+    window.location.replace(
+      `${REDIRECT_URL}?id=${storedId}&token=${storedToken}`
+    );
+  }, []);
   return <div>Dashboard</div>;
 };
 
